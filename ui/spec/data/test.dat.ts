21325c9fc199b3d396b9cc347e802ffaacde6cdf
HowePC <github@howepc.com>
2020-05-03 21:09:54 -0400

    add theory module and add test data content functions

--- ui/spec/data/test.dat.ts
+++ ui/spec/data/test.dat.ts
@@ -1,6 +1,29 @@
-// NOTE: Add new data fields as you see fit. They will be available in intellisense as properties of the data object.
-export const data = {
+// NOTE: Add new data type field and default data as you see fit.
+// These will be available in intellisense as properties of the interface object.
+
+export interface IProduct {
+	mainCategory: string;
+	subCategory: string;
+	productName: string;
+}
+
+const defaultProduct: IProduct = {
     mainCategory: "DRESSES",
     subCategory: "SUMMER DRESSES",
     productName: "Printed Chiffon Dress",
 };
+
+
+import { readCSV } from "../lib/helpers";
+
+export function getProducts(): IProduct[] {
+
+    let records: any;
+    const csvFile = ("./spec/data/" + process.env.TEST_DATA_CSV || "none");
+    if (csvFile === "none") {
+        records = [defaultProduct];
+    } else {
+        records = readCSV(csvFile);
+    }
+    return records;
+}

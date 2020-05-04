32e1522e3128df0ae307490dd4a77e94277d6d8d
HowePC <github@howepc.com>
2020-05-03 21:11:14 -0400

    Loop tests based on CSV

--- ui/spec/regression.test/shareProducts.spec.ts
+++ ui/spec/regression.test/shareProducts.spec.ts
@@ -1,6 +1,8 @@
 // Data
 import { env } from "../data/environment.dat";
-import { data } from "../data/test.dat";
+import { getProducts, IProduct } from "../data/test.dat";
+
+import { using } from "../lib/theory";
 
 // Page Objects
 import {
@@ -12,17 +14,20 @@ const productPg = new ProductPg();
 fixture `AutoPrac Product Social Media Sharing`
 .page(env.url);
 
-test("Verify social media sharing links", async (t) => {
-    await t.maximizeWindow();
-    await productPg.findItemDetail(data.productName);
+using (getProducts(), (product: IProduct) => {
 
-    await t
-    .expect(productPg.twitterButton.getAttribute("onclick"))
-        .contains("socialsharing_twitter_click('")
-    .expect(productPg.facebookButton.getAttribute("onclick"))
-        .contains("socialsharing_facebook_click();")
-    .expect(productPg.googlePlusButton.getAttribute("onclick"))
-        .contains("socialsharing_google_click();")
-    .expect(productPg.pinterestButton.getAttribute("onclick"))
-        .contains("socialsharing_pinterest_click('http://automationpractice.com/img/p/");
+    test("Verify social media sharing links", async (t) => {
+        await t.maximizeWindow();
+        await productPg.findItemDetail(product.productName);
+
+        await t
+        .expect(productPg.twitterButton.getAttribute("onclick"))
+            .contains("socialsharing_twitter_click('")
+        .expect(productPg.facebookButton.getAttribute("onclick"))
+            .contains("socialsharing_facebook_click();")
+        .expect(productPg.googlePlusButton.getAttribute("onclick"))
+            .contains("socialsharing_google_click();")
+        .expect(productPg.pinterestButton.getAttribute("onclick"))
+            .contains("socialsharing_pinterest_click('http://automationpractice.com/img/p/");
+    });
 });
